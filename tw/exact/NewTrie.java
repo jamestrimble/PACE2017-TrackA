d643739d411a309eebe26ba6740ea8d830d965cf
James Trimble <james.trimble@yahoo.co.uk>
2020-08-04 11:25:19 +0100

    Make more members private

--- tw/exact/NewTrie.java
+++ tw/exact/NewTrie.java
@@ -3,37 +3,36 @@ package tw.exact;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class NewTrie {
-    int graph_n;
-    int targetWidth;
-    TrieNode root;
+class NewTrie {
+    private int n;
+    private int targetWidth;
+    private TrieNode root;
 
-    public class TrieNode {
-        TrieNode[] successors = null;
-        XBitSet subtrieUnionOfSets;
-        XBitSet subtrieIntersectionOfNds;
-        int key;
-        XBitSet[] vals = null;
-        XBitSet nbs = null;
-        int graph_n;
+    private class TrieNode {
+        private TrieNode[] successors = null;
+        private XBitSet subtrieUnionOfSets;
+        private XBitSet subtrieIntersectionOfNds;
+        private int key;
+        private XBitSet[] vals = null;
+        private XBitSet nbs = null;
 
-        int numValsInSubtrie;
+        private int numValsInSubtrie;
+
         // As an optimisation, we store one (S, N(S)) pair that appears in this
         // subtrie.  If numValsInSubtrie==1, we can use this pair and avoid
         // recursing further at query time.
-        XBitSet verticesInSubTrie;
-        XBitSet nbsInSubTrie;
+        private XBitSet verticesInSubTrie;
+        private XBitSet nbsInSubTrie;
 
-        TrieNode(int key, int graph_n) {
+        TrieNode(int key, int n) {
             this.key = key;
-            this.graph_n = graph_n;
-            subtrieIntersectionOfNds = new XBitSet(graph_n);
-            subtrieUnionOfSets = new XBitSet(graph_n);
-            subtrieIntersectionOfNds.set(0, graph_n);
+            subtrieIntersectionOfNds = new XBitSet(n);
+            subtrieUnionOfSets = new XBitSet(n);
+            subtrieIntersectionOfNds.set(0, n);
         }
 
-        TrieNode addSuccessorNode(int key) {
-            TrieNode node = new TrieNode(key, graph_n);
+        private TrieNode addSuccessorNode(int key, int n) {
+            TrieNode node = new TrieNode(key, n);
             if (successors == null) {
                 successors = new TrieNode[1];
                 successors[0] = node;
@@ -44,7 +43,7 @@ public class NewTrie {
             return node;
         }
 
-        TrieNode getOrAddSuccessorNode(int key) {
+        TrieNode getOrAddSuccessorNode(int key, int n) {
             if (successors != null) {
                 for (TrieNode succ : successors) {
                     if (succ.key == key) {
@@ -52,7 +51,7 @@ public class NewTrie {
                     }
                 }
             }
-            return addSuccessorNode(key);
+            return addSuccessorNode(key, n);
         }
 
         private void recordSetInSubtrie(XBitSet vertices, XBitSet neighbours) {
@@ -108,19 +107,18 @@ public class NewTrie {
         }
     }
 
-    public NewTrie(int n, int targetWidth) {
-        this.graph_n = n;
+    NewTrie(int n, int targetWidth) {
+        this.n = n;
         this.targetWidth = targetWidth;
-
         root = new TrieNode(-1, n);
     }
 
-    public void put(XBitSet vertices, XBitSet neighbours) {
+    void put(XBitSet vertices, XBitSet neighbours) {
         root.recordSetInSubtrie(vertices, neighbours);
         TrieNode node = root;
         // iterate over elements of neighbours
         for (int i = neighbours.nextSetBit(0); i >= 0; i = neighbours.nextSetBit(i+1)) {
-            node = node.getOrAddSuccessorNode(i);
+            node = node.getOrAddSuccessorNode(i, n);
             node.recordSetInSubtrie(vertices, neighbours);
         }
         if (node.vals == null) {
@@ -133,12 +131,12 @@ public class NewTrie {
         }
     }
 
-    public void put(XBitSet vertices, int neighborSize, XBitSet neighbours) {
+    void put(XBitSet vertices, int neighborSize, XBitSet neighbours) {
         put(vertices, neighbours);
     }
 
     // for debugging
-    public void showList(ArrayList<XBitSet> bitsets) {
+    void showList(ArrayList<XBitSet> bitsets) {
         for (XBitSet bs : bitsets) {
             for (int i = bs.nextSetBit(0); i >= 0; i = bs.nextSetBit(i+1)) {
                 System.out.print(i + " ");
@@ -148,12 +146,12 @@ public class NewTrie {
         System.out.println("--------------");
     }
 
-    public void collectSuperblocks(XBitSet component, XBitSet neighbours,
+    void collectSuperblocks(XBitSet component, XBitSet neighbours,
             ArrayList<XBitSet> list) {
         root.getAllAlmostSubsetsHelper(component, neighbours, targetWidth + 1, targetWidth + 1 - neighbours.cardinality(), list);
     }
 
-    public int[] getSizes() {
+    int[] getSizes() {
         // a dummy implementation
         int sizes[] = new int[1];
         sizes[0] = -1;
