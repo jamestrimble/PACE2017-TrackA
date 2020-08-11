1b4f1ef6c95891dcef2dc528d13b1e6142d6a8d4
James <james.trimble@yahoo.co.uk>
2020-08-11 16:56:12 +0100

    Added results of a small experiment

--- experiment-on-random-instances/make_random_graphs.sh
+++ experiment-on-random-instances/make_random_graphs.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-for n in $(seq 10 10 100); do
-    for p in 1 5 9; do
-        for i in $(seq 1 10); do
-            python random_graph.py $n .$p > gr/gr-$n-$p-$i.gr
-        done
+p=1
+for n in $(seq 5 5 55); do
+    for i in $(seq 1 3); do
+        python random_graph.py $n .$p > gr/gr-$n-$p-$i.gr
+    done
+done
+p=5
+for n in $(seq 10 10 90); do
+    for i in $(seq 1 3); do
+        python random_graph.py $n .$p > gr/gr-$n-$p-$i.gr
+    done
+done
+p=9
+for n in $(seq 50 50 450); do
+    for i in $(seq 1 3); do
+        python random_graph.py $n .$p > gr/gr-$n-$p-$i.gr
     done
 done
