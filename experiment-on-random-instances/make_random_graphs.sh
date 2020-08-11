864058d38298750ce1e77a0da406112c04f580ce
James Trimble <james.trimble@yahoo.co.uk>
2020-08-11 20:35:44 +0100

    Modify script to create random graphs

--- experiment-on-random-instances/make_random_graphs.sh
+++ experiment-on-random-instances/make_random_graphs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 p=1
-for n in $(seq 5 5 55); do
-    for i in $(seq 1 3); do
+for n in $(seq 5 5 60); do
+    for i in $(seq 1 5); do
         python random_graph.py $n .$p > gr/gr-$n-$p-$i.gr
     done
 done
 p=5
-for n in $(seq 10 10 90); do
-    for i in $(seq 1 3); do
+for n in $(seq 10 10 100); do
+    for i in $(seq 1 5); do
         python random_graph.py $n .$p > gr/gr-$n-$p-$i.gr
     done
 done
 p=9
-for n in $(seq 50 50 450); do
-    for i in $(seq 1 3); do
+for n in $(seq 50 50 550); do
+    for i in $(seq 1 5); do
         python random_graph.py $n .$p > gr/gr-$n-$p-$i.gr
     done
 done
