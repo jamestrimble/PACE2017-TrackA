3b12147835f82e7f302f1c8a8c61524b63b2ca99
James Trimble <james.trimble@yahoo.co.uk>
2020-08-13 11:31:31 +0100

    Move trie node class to new file

--- tw/exact/NewTrie.java
+++ tw/exact/NewTrie.java
@@ -8,112 +8,6 @@ class NewTrie implements SupersetDataStructure, LatexPrintable {
     private TrieNode root;
     private int size;
 
-    private class TrieNode {
-        private TrieNode[] children = new TrieNode[0];
-        private XBitSet subtrieUnionOfSSets;
-        private XBitSet subtrieIntersectionOfNSets;
-        private int key;
-        private XBitSet[] SSets = new XBitSet[0];
-
-        TrieNode(int key, XBitSet initialIntersectionOfNSets) {
-            this.key = key;
-            subtrieIntersectionOfNSets = initialIntersectionOfNSets;
-            subtrieUnionOfSSets = new XBitSet();
-        }
-
-        TrieNode getOrAddChildNode(int key, XBitSet SSet, XBitSet NSet) {
-            for (TrieNode child : children) {
-                if (child.key == key) {
-                    child.subtrieIntersectionOfNSets.and(NSet);
-                    child.subtrieUnionOfSSets.or(SSet);
-                    return child;
-                }
-            }
-            // Node not found; add and return it
-            TrieNode node = new TrieNode(key, (XBitSet) NSet.clone());
-            node.subtrieUnionOfSSets.or(SSet);
-            children = Arrays.copyOf(children, children.length + 1);
-            children[children.length - 1] = node;
-            return node;
-        }
-
-        private void query(XBitSet queryS, XBitSet queryN, int k,
-                int budget, ArrayList<XBitSet> out_list) {
-            if (subtrieIntersectionOfNSets.subtract(queryN).cardinality() > k) {
-                return;
-            }
-            if (!queryS.isSubset(subtrieUnionOfSSets)) {
-                return;
-            }
-            for (XBitSet SSet : SSets) {
-                if (queryS.isSubset(SSet)) {
-                    out_list.add((XBitSet) subtrieIntersectionOfNSets.clone());
-                    break;
-                }
-            }
-            for (TrieNode child : children) {
-                int newBudget = queryN.get(child.key) ? budget : budget - 1;
-                if (newBudget >= 0) {
-                    child.query(queryS, queryN, k, newBudget, out_list);
-                }
-            }
-        }
-
-        private void printLatexBitset(XBitSet bitset, String colour) {
-            System.out.print("\\\\ [-1ex] \\scriptsize {\\color{" + colour + "} $");
-            if (bitset.isEmpty()) {
-                System.out.print("\\emptyset");
-            } else {
-                String comma = "";
-                for (int v = bitset.nextSetBit(0); v >= 0; v = bitset.nextSetBit(v+1)) {
-                    System.out.print(comma + v);
-                    comma = " ";
-                }
-            }
-            System.out.print("$} ");
-        }
-
-        private void printLatex(ArrayList<Integer> currentNodeN, int featureFlags) {
-            System.out.print("[{$");
-            if (currentNodeN.isEmpty()) {
-                System.out.print("\\emptyset");
-            } else {
-                for (int v : currentNodeN) {
-                    if (v == currentNodeN.get(currentNodeN.size() - 1)) {
-                        System.out.print("\\mathbf{\\underline{" + v + "}}");
-                    } else {
-                        System.out.print(v);
-                    }
-                }
-            }
-            System.out.print("$ ");
-
-            if (0 != (featureFlags & 1)) {
-                printLatexBitset(subtrieIntersectionOfNSets, "black!50");
-            }
-
-            if (0 != (featureFlags & 2)) {
-                printLatexBitset(subtrieUnionOfSSets, "blue");
-            }
-
-            if (0 != (featureFlags & 4)) {
-                for (XBitSet SSet : SSets) {
-                    printLatexBitset(SSet, "blue!50");
-                }
-            }
-            System.out.print("},align=center");
-            if (SSets.length > 0) {
-                System.out.print(",line width=.7mm");
-            }
-            for (TrieNode child : children) {
-                currentNodeN.add(child.key);
-                child.printLatex(currentNodeN, featureFlags);
-                currentNodeN.remove(currentNodeN.size() - 1);
-            }
-            System.out.print("]");
-        }
-    }
-
     public NewTrie(int n, int targetWidth) {
         this.targetWidth = targetWidth;
         XBitSet initialIntersectionOfNSets = new XBitSet();
@@ -129,8 +23,7 @@ class NewTrie implements SupersetDataStructure, LatexPrintable {
         for (int i = NSet.nextSetBit(0); i >= 0; i = NSet.nextSetBit(i+1)) {
             node = node.getOrAddChildNode(i, SSet, NSet);
         }
-        node.SSets = Arrays.copyOf(node.SSets, node.SSets.length + 1);
-        node.SSets[node.SSets.length - 1] = SSet;
+        node.addSSet(SSet);
         ++size;
     }
 
---
+++ tw/exact/TrieNode.java
@@ -0,0 +1,115 @@
+package tw.exact;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class TrieNode {
+    private TrieNode[] children = new TrieNode[0];
+    XBitSet subtrieUnionOfSSets;
+    XBitSet subtrieIntersectionOfNSets;
+    private int key;
+    private XBitSet[] SSets = new XBitSet[0];
+
+    TrieNode(int key, XBitSet initialIntersectionOfNSets) {
+        this.key = key;
+        subtrieIntersectionOfNSets = initialIntersectionOfNSets;
+        subtrieUnionOfSSets = new XBitSet();
+    }
+
+    void addSSet(XBitSet SSet) {
+        SSets = Arrays.copyOf(SSets, SSets.length + 1);
+        SSets[SSets.length - 1] = SSet;
+    }
+
+    TrieNode getOrAddChildNode(int key, XBitSet SSet, XBitSet NSet) {
+        for (TrieNode child : children) {
+            if (child.key == key) {
+                child.subtrieIntersectionOfNSets.and(NSet);
+                child.subtrieUnionOfSSets.or(SSet);
+                return child;
+            }
+        }
+        // Node not found; add and return it
+        TrieNode node = new TrieNode(key, (XBitSet) NSet.clone());
+        node.subtrieUnionOfSSets.or(SSet);
+        children = Arrays.copyOf(children, children.length + 1);
+        children[children.length - 1] = node;
+        return node;
+    }
+
+    void query(XBitSet queryS, XBitSet queryN, int k,
+            int budget, ArrayList<XBitSet> out_list) {
+        if (subtrieIntersectionOfNSets.subtract(queryN).cardinality() > k) {
+            return;
+        }
+        if (!queryS.isSubset(subtrieUnionOfSSets)) {
+            return;
+        }
+        for (XBitSet SSet : SSets) {
+            if (queryS.isSubset(SSet)) {
+                out_list.add((XBitSet) subtrieIntersectionOfNSets.clone());
+                break;
+            }
+        }
+        for (TrieNode child : children) {
+            int newBudget = queryN.get(child.key) ? budget : budget - 1;
+            if (newBudget >= 0) {
+                child.query(queryS, queryN, k, newBudget, out_list);
+            }
+        }
+    }
+
+    void printLatexBitset(XBitSet bitset, String colour) {
+        System.out.print("\\\\ [-1ex] \\scriptsize {\\color{" + colour + "} $");
+        if (bitset.isEmpty()) {
+            System.out.print("\\emptyset");
+        } else {
+            String comma = "";
+            for (int v = bitset.nextSetBit(0); v >= 0; v = bitset.nextSetBit(v+1)) {
+                System.out.print(comma + v);
+                comma = " ";
+            }
+        }
+        System.out.print("$} ");
+    }
+
+    void printLatex(ArrayList<Integer> currentNodeN, int featureFlags) {
+        System.out.print("[{$");
+        if (currentNodeN.isEmpty()) {
+            System.out.print("\\emptyset");
+        } else {
+            for (int v : currentNodeN) {
+                if (v == currentNodeN.get(currentNodeN.size() - 1)) {
+                    System.out.print("\\mathbf{\\underline{" + v + "}}");
+                } else {
+                    System.out.print(v);
+                }
+            }
+        }
+        System.out.print("$ ");
+
+        if (0 != (featureFlags & 1)) {
+            printLatexBitset(subtrieIntersectionOfNSets, "black!50");
+        }
+
+        if (0 != (featureFlags & 2)) {
+            printLatexBitset(subtrieUnionOfSSets, "blue");
+        }
+
+        if (0 != (featureFlags & 4)) {
+            for (XBitSet SSet : SSets) {
+                printLatexBitset(SSet, "blue!50");
+            }
+        }
+        System.out.print("},align=center");
+        if (SSets.length > 0) {
+            System.out.print(",line width=.7mm");
+        }
+        for (TrieNode child : children) {
+            currentNodeN.add(child.key);
+            child.printLatex(currentNodeN, featureFlags);
+            currentNodeN.remove(currentNodeN.size() - 1);
+        }
+        System.out.print("]");
+    }
+}
